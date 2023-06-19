78acd8ffa3b5ad9807529204bfb2e2e31ba0950f
skysmyoo7 <skysmyoo7@gmail.com>
2023-06-19 11:19:44 +0900

    로그인 화면 레이아웃 구성

---
+++ app/src/main/java/com/skysmyoo/publictalk/BaseFragment.kt
@@ -0,0 +1,35 @@
+package com.skysmyoo.publictalk
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+open class BaseFragment : Fragment() {
+
+    protected var _binding: ViewDataBinding? = null
+    open val binding get() = _binding!!
+    open val layoutId: Int = 0
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
