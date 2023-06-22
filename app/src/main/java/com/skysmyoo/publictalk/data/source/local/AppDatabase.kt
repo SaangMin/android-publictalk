677a017ffb5a7a84040569465052dea78034527d
skysmyoo7 <skysmyoo7@gmail.com>
2023-06-22 16:11:34 +0900

    Room 기본 설정

---
+++ app/src/main/java/com/skysmyoo/publictalk/data/source/local/AppDatabase.kt
@@ -0,0 +1,12 @@
+package com.skysmyoo.publictalk.data.source.local
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.skysmyoo.publictalk.data.model.remote.request.User
+
+@Database(entities = [User::class], version = 1)
+@TypeConverters(Converters::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun userModelDao(): UserModelDao
+}
