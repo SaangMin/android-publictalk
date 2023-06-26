964bd0722d1b2dcdc57cef914406776dd23c1287
skysmyoo7 <skysmyoo7@gmail.com>
2023-06-26 15:37:08 +0900

    로그인 화면 MVVM 패턴 적용

--- app/src/main/java/com/skysmyoo/publictalk/data/source/local/UserLocalDataSource.kt
+++ app/src/main/java/com/skysmyoo/publictalk/data/source/local/UserLocalDataSource.kt
@@ -1,11 +1,19 @@
 package com.skysmyoo.publictalk.data.source.local
 
+import com.skysmyoo.publictalk.PublicTalkApplication.Companion.preferencesManager
 import com.skysmyoo.publictalk.data.model.remote.User
+import com.skysmyoo.publictalk.data.source.remote.FirebaseData.setUserInfo
 
 class UserLocalDataSource(private val userModelDao: UserModelDao) {
 
     suspend fun insertUser(user: User) {
+        userModelDao.clearUser()
+        with(preferencesManager) {
+            saveMyEmail(user.userEmail)
+            setLocale(user.userLanguage)
+        }
         userModelDao.insertUser(user)
+        setUserInfo()
     }
 
     suspend fun getMyInfo(email: String): User? {
