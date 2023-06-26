8b3072a510166b07619ab77a55670acbbcf93ada
skysmyoo7 <skysmyoo7@gmail.com>
2023-06-26 17:54:02 +0900

    테스트용 샘플 데이터 추가

---
+++ app/src/main/java/com/skysmyoo/publictalk/data/model/local/TestSampleData.kt
@@ -0,0 +1,17 @@
+package com.skysmyoo.publictalk.data.model.local
+
+import com.skysmyoo.publictalk.data.model.remote.User
+
+object TestSampleData {
+
+    val sampleUser = User(
+        userEmail = "iu@gmail.com",
+        userName = "아이유",
+        userPhoneNumber = "01012345678",
+        userProfileImage = "https://i.namu.wiki/i/R0AhIJhNi8fkU2Al72pglkrT8QenAaCJd1as-d_iY6MC8nub1iI5VzIqzJlLa-1uzZm--TkB-KHFiT-P-t7bEg.webp",
+        userLanguage = "ko",
+        userDeviceToken = "",
+        userFriendIdList = null,
+        userCreatedAt = "2023-06-26 15:16:38"
+    )
+}
