20b99037e7317efdb7efe45d8373b9104c35b85a
skysmyoo7 <skysmyoo7@gmail.com>
2023-06-26 17:59:14 +0900

    Message 데이터 구조 변경

--- app/src/main/java/com/skysmyoo/publictalk/data/model/remote/Message.kt
+++ app/src/main/java/com/skysmyoo/publictalk/data/model/remote/Message.kt
@@ -1,8 +1,9 @@
 package com.skysmyoo.publictalk.data.model.remote
 
 data class Message(
-    val me: User,
-    val other: User,
+    val sender: User,
+    val receiver: User,
     val body: String,
+    val isReading: Boolean,
     val createdAt: String,
 )
