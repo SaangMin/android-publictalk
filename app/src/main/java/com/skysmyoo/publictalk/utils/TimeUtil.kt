b5b8a74d372a0c43688b3fa0631b314479ba17a5
skysmyoo7 <skysmyoo7@gmail.com>
2023-07-07 09:12:58 +0900

    국가 지역별 시간 다르게 설정되는 이슈 해결

--- app/src/main/java/com/skysmyoo/publictalk/utils/TimeUtil.kt
+++ app/src/main/java/com/skysmyoo/publictalk/utils/TimeUtil.kt
@@ -3,7 +3,6 @@ package com.skysmyoo.publictalk.utils
 import android.content.res.Resources
 import android.os.Build
 import java.text.SimpleDateFormat
-import java.util.Calendar
 import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
@@ -24,33 +23,30 @@ object TimeUtil {
 
     fun getCurrentDateString(): String {
         val date = Date()
-        val sdf = SimpleDateFormat(DATE_YEAR_MONTH_DAY_TIME_PATTERN, Locale.getDefault())
-        sdf.timeZone = TimeZone.getTimeZone("UTC")
-        return sdf.format(date)
+        val utcZone = TimeZone.getTimeZone("UTC")
+        val simpleDateFormat = SimpleDateFormat(DATE_YEAR_MONTH_DAY_TIME_PATTERN, Locale.ROOT)
+        simpleDateFormat.timeZone = utcZone
+        return simpleDateFormat.format(date)
     }
 
     fun convertDateTime(dateTimeString: String): String {
         val date = dateTimeString.toDate(DATE_YEAR_MONTH_DAY_TIME_PATTERN)
-
         val converterPattern = if (date.isToday()) TIME_PATTERN else DATE_AND_TIME_PATTERN
         val dateFormat = SimpleDateFormat(converterPattern, currentLocale)
-
         return dateFormat.format(date)
     }
 
     private fun String.toDate(pattern: String): Date {
-        val dateFormat = SimpleDateFormat(pattern, currentLocale)
+        val dateFormat = SimpleDateFormat(pattern, Locale.ROOT)
+        dateFormat.timeZone = TimeZone.getTimeZone("UTC")
         return dateFormat.parse(this) ?: Date()
     }
 
     private fun Date.isToday(): Boolean {
-        val calendarTarget = Calendar.getInstance()
-        calendarTarget.time = this
+        val dateFormat = SimpleDateFormat("yyyyMMdd", Locale.ROOT)
+        val targetDateString = dateFormat.format(this)
+        val currentDateString = dateFormat.format(Date())
 
-        val calendar = Calendar.getInstance()
-
-        return (calendarTarget.get(Calendar.YEAR) == calendar.get(Calendar.YEAR)
-                && calendarTarget.get(Calendar.MONTH) == calendar.get(Calendar.MONTH)
-                && calendarTarget.get(Calendar.DAY_OF_MONTH) == calendar.get(Calendar.DAY_OF_MONTH))
+        return targetDateString.toInt() <= currentDateString.toInt()
     }
 }
