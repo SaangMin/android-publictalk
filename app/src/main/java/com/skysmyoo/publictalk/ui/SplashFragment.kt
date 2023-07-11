c8143201c06051e02ce836cc6297e72e3f8951d8
skysmyoo7 <skysmyoo7@gmail.com>
2023-07-11 17:32:31 +0900

    네트워크 상태 확인 후 자동 로그인 기능 구현

--- app/src/main/java/com/skysmyoo/publictalk/ui/SplashFragment.kt
+++ app/src/main/java/com/skysmyoo/publictalk/ui/SplashFragment.kt
@@ -67,6 +67,23 @@ class SplashFragment : BaseFragment() {
         }
     }
 
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = requireContext().getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork) ?: return false
+            return when {
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+                capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> true
+                else -> false
+            }
+        } else {
+            val activeNetworkInfo = connectivityManager.activeNetworkInfo
+            return activeNetworkInfo != null && activeNetworkInfo.isConnected
+        }
+    }
+
     private fun navigateToLogin() {
         val action = SplashFragmentDirections.actionSplashToLogin()
         findNavController().navigate(action)
