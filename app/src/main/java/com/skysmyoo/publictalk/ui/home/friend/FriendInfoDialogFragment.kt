90c24f14205ec82f5312754cba47ace08476c257
skysmyoo7 <skysmyoo7@gmail.com>
2023-07-11 10:34:01 +0900

    친구 정보 화면 레이아웃 구성

---
+++ app/src/main/java/com/skysmyoo/publictalk/ui/home/friend/FriendInfoDialogFragment.kt
@@ -0,0 +1,96 @@
+package com.skysmyoo.publictalk.ui.home.friend
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.skysmyoo.publictalk.data.model.remote.ChatRoom
+import com.skysmyoo.publictalk.data.model.remote.ChattingMember
+import com.skysmyoo.publictalk.data.model.remote.User
+import com.skysmyoo.publictalk.databinding.DialogFriendInfoBinding
+import com.skysmyoo.publictalk.ui.home.HomeViewModel
+import com.skysmyoo.publictalk.utils.EventObserver
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FriendInfoDialogFragment : DialogFragment() {
+
+    private var _binding: DialogFriendInfoBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: HomeViewModel by viewModels()
+    private val args: FriendInfoDialogFragmentArgs by navArgs()
+    private lateinit var friendInfo: User
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = DialogFriendInfoBinding.inflate(inflater, container, false)
+        friendInfo = args.user
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            friend = friendInfo
+            btnFriendInfoOpenChat.setOnClickListener {
+                getChatRoom()
+            }
+        }
+        notExistChatRoomObserver()
+        foundChatRoomObserver()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+    }
+
+    private fun getChatRoom() {
+        val myEmail = viewModel.getMyEmail()
+        val member = listOf(myEmail, friendInfo.userEmail).sorted()
+        viewModel.getChatRoom(member)
+    }
+
+    private fun notExistChatRoomObserver() {
+        viewModel.notExistChatRoom.observe(viewLifecycleOwner, EventObserver {
+            val myEmail = viewModel.getMyEmail()
+            val member = listOf(
+                ChattingMember(userEmail = myEmail),
+                ChattingMember(userEmail = friendInfo.userEmail)
+            )
+            val newChatRoom = ChatRoom(member = member)
+            val action = FriendInfoDialogFragmentDirections.actionFriendInfoToChatRoom(newChatRoom)
+            with(findNavController()) {
+                navigate(action)
+                popBackStack()
+            }
+        })
+    }
+
+    private fun foundChatRoomObserver() {
+        viewModel.foundChatRoom.observe(viewLifecycleOwner, EventObserver {
+            val action = FriendInfoDialogFragmentDirections.actionFriendInfoToChatRoom(it)
+            with(findNavController()) {
+                navigate(action)
+                popBackStack()
+            }
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
