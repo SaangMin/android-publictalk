926e6032011c6f75d0e64278036d83507018f530
skysmyoo7 <skysmyoo7@gmail.com>
2023-07-19 09:28:53 +0900

    알림 기능 BroadcastReceiver에서 관리

--- app/src/main/java/com/skysmyoo/publictalk/receiver/MyBroadcastReceiver.kt
+++ app/src/main/java/com/skysmyoo/publictalk/receiver/MyBroadcastReceiver.kt
@@ -1,5 +1,6 @@
 package com.skysmyoo.publictalk.receiver
 
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Context.VIBRATOR_SERVICE
@@ -9,12 +10,17 @@ import android.os.VibrationEffect
 import android.os.Vibrator
 import android.os.VibratorManager
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.skysmyoo.publictalk.R
 import com.skysmyoo.publictalk.utils.Constants
 
 class MyBroadcastReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == Constants.MY_NOTIFICATION) {
+            val title = intent.getStringExtra(Constants.KEY_MESSAGE_TITLE)
+            val body = intent.getStringExtra(Constants.KEY_MESSAGE_BODY)
+            showNotification(context, title, body)
+
             val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                 val vibratorManager =
                     context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
@@ -38,6 +44,18 @@ class MyBroadcastReceiver : BroadcastReceiver() {
                 Toast.LENGTH_SHORT
             ).show()
         }
+    }
+
+    private fun showNotification(context: Context, title: String?, body: String?) {
+        val notificationBuilder = NotificationCompat.Builder(context, "channel_id")
+            .setSmallIcon(R.drawable.icon_logo_min)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setAutoCancel(true)
+
+        val notificationManager =
+            context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notificationBuilder.build())
     }
 
     companion object {
