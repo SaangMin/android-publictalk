f8eff4fbff8ed3e7a6c86894b6b8aaf04da41884
skysmyoo7 <skysmyoo7@gmail.com>
2023-07-27 16:13:46 +0900

    사용하지 않는 파일 및 코드 삭제

--- app/src/main/java/com/skysmyoo/publictalk/ui/theme/Color.kt
+++ app/src/main/java/com/skysmyoo/publictalk/ui/theme/Color.kt
@@ -2,14 +2,6 @@ package com.skysmyoo.publictalk.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
 val CancelColor = Color(0xFFFF5449)
 val SubmitColor = Color(0xFF6A5E2F)
 val DialogBackground = Color(0xFF998E8B)
