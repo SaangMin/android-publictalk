43b32329af894706058ad1557ea96037355bbdcf
skysmyoo7 <skysmyoo7@gmail.com>
2023-07-27 15:00:25 +0900

    불필요한 함수 구현 삭제

--- app/src/main/java/com/skysmyoo/publictalk/data/source/remote/FcmClient.kt
+++ app/src/main/java/com/skysmyoo/publictalk/data/source/remote/FcmClient.kt
@@ -1,19 +1,12 @@
 package com.skysmyoo.publictalk.data.source.remote
 
 import com.skysmyoo.publictalk.data.model.remote.FcmNotification
-import com.skysmyoo.publictalk.data.model.remote.Token
 import com.skysmyoo.publictalk.data.source.remote.response.ApiResponse
-import okhttp3.ResponseBody
 import retrofit2.http.Body
 import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface FcmClient {
-
-    @POST("fcm/register")
-    suspend fun registerToken(
-        @Body token: Token
-    ): ApiResponse<ResponseBody>
 
     @POST("fcm/send")
     suspend fun sendNotification(
