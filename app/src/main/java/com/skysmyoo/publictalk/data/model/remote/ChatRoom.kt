ee5f8d4d49f4b5ca5cb9e5ff3e2ab05bfbdfb54a
skysmyoo7 <skysmyoo7@gmail.com>
2023-08-03 11:58:39 +0900

    채팅방 overView 상태에도 종료되지 않게 구현

--- app/src/main/java/com/skysmyoo/publictalk/data/model/remote/ChatRoom.kt
+++ app/src/main/java/com/skysmyoo/publictalk/data/model/remote/ChatRoom.kt
@@ -1,17 +1,19 @@
 package com.skysmyoo.publictalk.data.model.remote
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.google.firebase.database.PropertyName
 import com.squareup.moshi.JsonClass
-import java.io.Serializable
+import kotlinx.parcelize.Parcelize
 
 @Entity(tableName = "saved_chat_rooms")
 @JsonClass(generateAdapter = true)
+@Parcelize
 data class ChatRoom(
     @PrimaryKey(autoGenerate = true)
     @PropertyName("uid") val uid: Int = 0,
     @PropertyName("member") val member: List<ChattingMember> = emptyList(),
     @PropertyName("messages") val messages: Map<String, Message> = emptyMap(),
     @PropertyName("chatCreatedAt") val chatCreatedAt: String = "",
-) : Serializable
+) : Parcelable
